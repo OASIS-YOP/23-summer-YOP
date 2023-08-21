12676a8126895f61c8fe9907dd3bf37f4c21a778
urimJ <ufrl128@dgu.ac.kr>
2023-08-22 01:21:59 +0900

    Feat: amend scaleControl variable on ImageTab

--- src/components/TabMenuContainer/TabMenu/ImageTab/index.jsx
+++ src/components/TabMenuContainer/TabMenu/ImageTab/index.jsx
@@ -94,29 +94,29 @@ export const ImageTab = ({ canvas, image }) => {
 
   const scaleControl = () => {
     const scale = document.getElementById('scale-control');
-    if (image) {
-      const currentCenter = image.getCenterPoint();
+    // if (image) {
+    //   const currentCenter = image.getCenterPoint();
 
-      // 이미지의 회전 중심 변경
-      image.set({
-        originX: 'center',
-        originY: 'center',
-      });
+    //   // 이미지의 회전 중심 변경
+    //   image.set({
+    //     originX: 'center',
+    //     originY: 'center',
+    //   });
 
-      //변한 이미지의 회전중심 얻기
-      const newCenter = image.getCenterPoint();
-      //회전 중심 조정
-      const deltaX = currentCenter.x - newCenter.x;
-      const deltaY = currentCenter.y - newCenter.y;
+    //   //변한 이미지의 회전중심 얻기
+    //   const newCenter = image.getCenterPoint();
+    //   //회전 중심 조정
+    //   const deltaX = currentCenter.x - newCenter.x;
+    //   const deltaY = currentCenter.y - newCenter.y;
 
-      image.set({
-        left: image.left + deltaX,
-        top: image.top + deltaY,
-      });
+    //   image.set({
+    //     left: image.left + deltaX,
+    //     top: image.top + deltaY,
+    //   });
 
       image.scale(parseFloat(scale.value) / 50).setCoords();
       canvas.requestRenderAll();
-    }
+    // }
   };
 
   const topControl = () => {
