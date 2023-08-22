6e59ef10f50d0e31da68806c3e7d9f33f5c05d50
urimJ <ufrl128@dgu.ac.kr>
2023-08-23 00:31:45 +0900

    Fix: make typewriter speed faster

--- src/components/HeaderNavContents/index.jsx
+++ src/components/HeaderNavContents/index.jsx
@@ -173,6 +173,8 @@ export const HeaderNavContents = () => {
     handleChangedFile;
   }, [image]);
 
+  
+
   // const moveBackgroundImage = (leftOffset, topOffset) => {
   //   if (canvas.backgroundImage) {
   //     // 현재 배경 이미지의 위치 가져오기
@@ -528,19 +530,20 @@ export const HeaderNavContents = () => {
         <s.Content className={toggleState === 1 ? 'active' : ''} id='info'>
         <s.InfoContainer>
             <s.Title>온폴(Y.O.P) 프로젝트란?</s.Title>
-            <s.Info>
-            <Typewriter
-              // options={{ autoStart: true, loop: false }}
-              onInit={(typewriter) => {
-                typewriter
-
-                  .typeString("<br/>온폴(Your Own Polaroid)은 팀 오아시스의 온라인 폴라로이드 꾸미기 솔루션입니다. <br/> 온폴을 통해서, 인터넷과 브라우저만 있으면 온라인 상에서 폴라로이드 꾸미기가 가능합니다.<br/><br/> 회원가입도 비용 지불도 없이 온폴에서 제공하는 무궁무진한 오픈소스를 사용해보세요.<br/> 이미지를 규격에 맞춰 자르고 자유롭게 편집하여 당신만의 폴라로이드 사진을 직접 제작해보세요!")
-
-                  .pauseFor(100)
-
-                  .start();
-              }}
-            />
+            <s.Info id="info">
+            {toggleState === 1 && ( // toggleState가 1일 때에만 Typewriter를 시작
+                <Typewriter
+                  options={{ delay: 75 }}
+                  onInit={(typewriter) => {
+                    typewriter
+                      .typeString(
+                        "<br/>온폴(Your Own Polaroid)은 팀 오아시스의 온라인 폴라로이드 꾸미기 솔루션입니다. <br/> 온폴을 통해서, 인터넷과 브라우저만 있으면 온라인 상에서 폴라로이드 꾸미기가 가능합니다.<br/><br/> 회원가입도 비용 지불도 없이 온폴에서 제공하는 무궁무진한 오픈소스를 사용해보세요.<br/> 이미지를 규격에 맞춰 자르고 자유롭게 편집하여 당신만의 폴라로이드 사진을 직접 제작해보세요!"
+                      )
+                      .pauseFor(100)
+                      .start();
+                  }}
+                />
+              )}
             </s.Info>
           </s.InfoContainer>
         </s.Content>
