c1fb7d6350e5513fcfb76299f8df76d58ae7813b
soojeonghan <tnwjd7890@gmail.com>
2023-08-23 01:05:42 +0900

    Design: PaintTab UI #70

--- src/components/TabMenuContainer/TabMenu/PaintTab/styles.js
+++ src/components/TabMenuContainer/TabMenu/PaintTab/styles.js
@@ -2,29 +2,33 @@ import styled from 'styled-components';
 import { pointColor, whiteColor } from '../../../../GlobalStyles';
 
 export const PaintContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  & button {
+    font-size: 1.7vh;
+    margin: 0.4rem 0.5rem;
+    border-radius: 0.5rem;
+    border: 1px solid #ccc;
+    height: 1.6rem;
 
-& > button {
-  font-size: 1.7vh;
-  margin: 0.4rem 0.5rem;
-  border-radius: 0.5rem;
-  border: 1px solid #ccc;
-  height: 1.6rem;
+    &:hover {
+      background-color: ${pointColor};
+      cursor: pointer;
+    }
 
-  &:hover {
-    background-color: ${pointColor};
-    cursor: pointer;
+    &:active {
+      background-color: ${whiteColor};
+    }
   }
+`;
+export const BtnChangeColor = styled.button``;
 
-  &:active {
-    background-color: ${whiteColor};
-  }
-}
-`
-export const BtnChangeColor = styled.button`
+export const BtnAddObj = styled.button``;
+export const BtnFixObj = styled.button``;
 
-`
-
-export const BtnAddObj = styled.button`
-`
-export const BtnFixObj = styled.button`
-`
+export const ColorpickerWrapper = styled.div`
+  width: fit-content;
+  height: fit-content;
+  margin: 1vh auto;
+`;
